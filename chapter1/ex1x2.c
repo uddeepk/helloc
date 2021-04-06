d703bdbef5843d44db5e660fb2f0f9e75ed80faa
uddeepk <uddeepk@gmail.com>
2021-04-06 11:14:32 -0800

    commented the possible errors

---
+++ chapter1/ex1x2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main ()
+{
+  //  printf("\cHello World!\n"); // warning: unknown escape sequence: '\c'
+  return 0;
+}
---
+++ chapter1/exercise1x1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+  // printf("Hello World!\n") //error: expected ';' before 'return'
+  //  printf"HelloWorld!\n"); // expected ';' before string constant (points to " between printf and HelloWorld
+  //expected statement before ')' token
+
+  //  printf(Hello World!\n"); // 'Hello' undeclared (first use in this function); did you mean 'ftello'?
+  // each undeclared identifier is reported only once for each function it appears in
+  // stray '\' in program
+  // missing terminating " character
+  // expected ';' before '}'
+  return 0;
+}
