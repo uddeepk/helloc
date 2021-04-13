de567e33cfafcbe09435b9daa23c877a0de16c57
uddeepk <uddeepk@gmail.com>
2021-04-13 11:43:42 -0800

    removes c style comments

---
+++ chapter1/ex1x23.c
@@ -0,0 +1,51 @@
+/* WAP to remove all comments from a C program */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define IN_COMMENT 1
+#define OUT_OF_COMMENT 0
+int main()
+{
+  char * buffer = NULL;
+  size_t len = 0;
+  ssize_t nread;
+  int c ;
+  int state = 0;
+    
+  while ((c = getchar()) != EOF) {
+    if ( state== IN_COMMENT) {
+      // Look for end
+      if ( c == '*') {
+	c = getchar();
+	if ( c == '/') {
+	  state = OUT_OF_COMMENT;
+	}
+      }
+      // Also, ignore chars
+    }
+    else {
+      // Look for beginning of comment
+      if ( c == '/' ) {
+	char n = getchar();
+	if ( n == '/') {
+	  // RUn through till '\n'
+	}
+	else if (  n == '*') {
+	  state = IN_COMMENT;
+	}
+	else {
+	  putchar(c);
+	  putchar(n);
+	}
+      }
+      else {
+	// Display characters
+	putchar (c);
+      }
+     
+    }
+  }
+  return 0;
+}
