0c26457873f88e7c9e9ab4b5c4ab46b15929b7ca
uddeepk <uddeepk@gmail.com>
2021-04-30 19:30:57 -0800

    itob with reverse using pointers

---
+++ chapter3/ex3x5.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+
+void itob(int n, char* s, int b);
+char getDigit(int n);
+void reverse (char* s);
+
+int main () {
+  int inputNumber, base;
+  char numberInBase[1000];
+  itob(15, numberInBase, 16);
+  printf("%s\n", numberInBase);
+  return 0;
+}
+
+void itob(int n, char* s, int b) {
+  int i, sign;
+
+  if (( sign = n) < 0) 
+    n = -n;
+  i = 0;
+  do {
+    int digit = n % b ;
+    char d = getDigit (digit);
+    s[i++] = d;
+  } while ( (n /= b) > 0);
+  if (sign < 0)
+    s[i++] = '-';
+  s[i] = '\0';
+  //strrev(s);
+  reverse(s);
+}
+
+char getDigit(int n) {
+  if( n < 10)
+    return n + '0';
+  else
+    return (n - 10) + 'a';
+}
+
+void reverse(char * s) {
+  char *begin, *end, temp;
+  int len, i;
+
+  len = strlen(s);
+
+  for ( i = 0 ; i < len - 1; ++i )
+    end++;
+
+  for ( i = 0 ; i < len / 2 ; ++i) {
+    temp = *end;
+    *end = * begin;
+    *begin = temp;
+
+    ++begin;
+    --end;
+  }
+}
